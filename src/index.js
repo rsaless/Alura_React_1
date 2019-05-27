808257a988b54112e7505d4640c09cf38dcd8d14
rsaless <rspavarina@gmail.com>
2019-05-27 05:03:44 -0300

    Implementação do react-router e rotas filhas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import AutorBox from './Autor';
+import Home from './Home';
+import './index.css';
+import {Router,Route,browserHistory,IndexRoute} from 'react-router';
 
 ReactDOM.render(
-    <App />, 
+    (<Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home}/>
+            <Route path="/autor" component={AutorBox}/>
+            <Route path="/livro"/>
+        </Route>
+    </Router>),
     document.getElementById('root')
 );
