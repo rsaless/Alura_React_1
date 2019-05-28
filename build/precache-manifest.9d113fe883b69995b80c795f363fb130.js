09f8cdb586d8fdf7f2f46c0de78b8045fd72f4fb
rsaless <rspavarina@gmail.com>
2019-05-27 23:11:33 -0300

    Alterações para deploy no heroku

---
+++ build/precache-manifest.9d113fe883b69995b80c795f363fb130.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce10224fd7047ea30bbd2aecd5868080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89dd21f242cb29f98553",
+    "url": "/static/css/main.0c937624.chunk.css"
+  },
+  {
+    "revision": "64d59b73d5e46e9b23ca",
+    "url": "/static/js/2.c89ef32c.chunk.js"
+  },
+  {
+    "revision": "89dd21f242cb29f98553",
+    "url": "/static/js/main.a1f665e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
